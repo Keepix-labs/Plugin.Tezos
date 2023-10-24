49976124a991badabd77835d4e7cad9680ddf9df
yamidevs <yamisaaf@live.fr>
2023-10-24 20:07:24 +0200

    Ajoutez des fichiers projet.

---
+++ src/Commands/Setup.cs
@@ -0,0 +1,25 @@
+﻿using Keepix.PluginSystem;
+using Plugin.Tezos.src.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Tezos.Commands
+{
+    public class Setup
+    {
+        [KeepixPluginFn("pre-install")]
+        public static async Task<bool> OnPreInstall()
+        {
+            return true;
+        }
+
+        [KeepixPluginFn("install")]
+        public static async Task<bool> OnInstall()
+        {
+            return await Docker.ExecuteComposeAsync("up -d ");
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Keepix.PluginSystem;
+using System.Reflection;
+
+namespace Plugin.Tezos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string arg = args.Count() > 0 ? args[0] : "";
+
+            Task task = KeepixPlugin.Run(arg, Assembly.GetExecutingAssembly().GetTypes());
+            task.Wait();
+        }
+    }
+}
